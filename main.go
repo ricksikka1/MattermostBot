3a6d53c1a789733bd696dc7ae4e50b1d6f3009e9
Rick.Sikka <rick.sikka@tulip.com>
2020-03-26 13:11:09 -0400

    added printf

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"fmt"
 	"github.com/ricksikka1/MattermostBot/mattermost"
 )
 
@@ -16,6 +17,8 @@ func main(){
 	api := mattermost.NewMatterMostClient(serverURL, botUserName, botPassword)
 
 	members := mattermost.GetChannelMembers(*api, teamName, channelName)
+	fmt.Printf("%+v\n", members)
+	fmt.Printf("There are %d members in channel %s for team %s\n", len(*members), channelName, teamName)
 
 	bot := mattermost.GetBotUser(*api)
 
